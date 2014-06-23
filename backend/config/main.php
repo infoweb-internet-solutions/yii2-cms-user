4165ce96e33a832aac6429ce9a29b3faeca2e045
Ruben Heymans <ruben.heymans@gmail.com>
2014-06-23 14:52:49 +0200

    Initial commit

---
+++ backend/config/main.php
@@ -0,0 +1,42 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log', 'admin'],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'allowActions' => [
+                'admin/*', // add or remove allowed actions to this list
+                '*',
+            ]
+        ],
+    ],
+    'components' => [
+        /*'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],*/
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
---
+++ common/config/main.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
+        ],
+
+
+    ],
+
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'allowUnconfirmedLogin' => true,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['infoweb']
+        ],
+
+
+
+    ]
+];
