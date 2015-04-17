d13890ad8defdb8040639968c1a2edb2035dba63
Fabio Maggio <fabio@infoweb.be>
2015-04-17 09:25:28 +0200

    Add profession constants to user model
    The constants are also used as the key for the professions array that can
    be retrieved from the model.

--- models/frontend/User.php
+++ models/frontend/User.php
@@ -10,6 +10,19 @@ class User extends BaseUser
     // Salutation constants
     const SALUTATION_MR = 'mr';
     const SALUTATION_MS = 'ms';
+    
+    // Workplacetype constants
+    const WORKPLACETYPE_HOSPITAL    = 'hospital';
+    const WORKPLACETYPE_PRIVATE     = 'private';
+    
+    // Profession constants
+    const PROFESSION_PNEUMOLOGIST   = 'pneumologist';
+    const PROFESSION_ALLERGIST      = 'allergist';
+    const PROFESSION_NKO            = 'nko';
+    const PROFESSION_INTERNIST      = 'internist';
+    const PROFESSION_DOCTOR         = 'doctor';
+    const PROFESSION_NURSE          = 'nurse';
+    const PROFESSION_PHARMACIST     = 'pharmacist';
         
     public $identityClass = 'infoweb\user\models\User';
     
@@ -23,13 +36,13 @@ class User extends BaseUser
     public static function professions()
     {
         return [
-            'pneumologist'          => Yii::t('infoweb/user', 'Pneumologist'),
-            'allergist'             => Yii::t('infoweb/user', 'Allergist'),
-            'nko'                   => Yii::t('infoweb/user', 'NKO'),
-            'internist'             => Yii::t('infoweb/user', 'Internist'),
-            'doctor'                => Yii::t('infoweb/user', 'Doctor'),
-            'nurse'                 => Yii::t('infoweb/user', 'Nurse'),
-            'pharmacist'            => Yii::t('infoweb/user', 'Pharmacist')
+            self::PROFESSION_PNEUMOLOGIST   => Yii::t('infoweb/user', 'Pneumologist'),
+            self::PROFESSION_ALLERGIST      => Yii::t('infoweb/user', 'Allergist'),
+            self::PROFESSION_NKO            => Yii::t('infoweb/user', 'NKO'),
+            self::PROFESSION_INTERNIST      => Yii::t('infoweb/user', 'Internist'),
+            self::PROFESSION_DOCTOR         => Yii::t('infoweb/user', 'Doctor'),
+            self::PROFESSION_NURSE          => Yii::t('infoweb/user', 'Nurse'),
+            self::PROFESSION_PHARMACIST     => Yii::t('infoweb/user', 'Pharmacist')
         ];
     }    
 }
