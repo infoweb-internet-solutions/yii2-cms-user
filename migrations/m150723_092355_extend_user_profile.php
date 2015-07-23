8815632617674385a337eb8d5002d055b7f9c16d
Fabio Maggio <fabio@infoweb.be>
2015-07-23 11:45:22 +0200

    Create migration for extending the profile
    The default user profile contains not enough fields. A couple of commonly
    used one's are added with this migration.

---
+++ migrations/m150723_092355_extend_user_profile.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150723_092355_extend_user_profile extends Migration
+{
+    public function safeUp()
+    {
+        // Get the column names of the current profile table
+        $columnNames = $this->db->schema->getTableSchema('profile', true)->getColumnNames();
+        $newColumns = ['firstname', 'address', 'city', 'zipcode', 'phone', 'mobile', 'fax', 'language'];
+        
+        // Drop any of the new columns if they exist yet
+        foreach ($newColumns as $newColumn) {
+            if (in_array($newColumn, $columnNames))
+                $this->dropColumn('{{%profile}}', $newColumn);
+        }
+        
+        // Add new columns
+        $this->addColumn('{{%profile}}', 'firstname', Schema::TYPE_STRING.'(255) NOT NULL');
+        $this->addColumn('{{%profile}}', 'address', Schema::TYPE_STRING.'(255) NOT NULL');
+        $this->addColumn('{{%profile}}', 'city', Schema::TYPE_STRING.'(255) NOT NULL');
+        $this->addColumn('{{%profile}}', 'zipcode', Schema::TYPE_STRING.'(25) NOT NULL');
+        $this->addColumn('{{%profile}}', 'phone', Schema::TYPE_STRING.'(25) NOT NULL');
+        $this->addColumn('{{%profile}}', 'mobile', Schema::TYPE_STRING.'(25) NOT NULL');
+        $this->addColumn('{{%profile}}', 'fax', Schema::TYPE_STRING.'(25) NOT NULL');
+        $this->addColumn('{{%profile}}', 'language', Schema::TYPE_STRING.'(2) NOT NULL DEFAULT \'nl\'');   
+    }
+
+    public function safeDown()
+    {
+        return false;    
+    }
+}
