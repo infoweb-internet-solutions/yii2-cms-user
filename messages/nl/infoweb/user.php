64f525a827c691f6f4063ce0a79517199f5882c3
Ruben Heymans <ruben.heymans@gmail.com>
2015-07-28 14:30:54 +0200

    Add translations

---
+++ messages/nl/infoweb/user.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'Address' => 'Adres',
+    'City' => 'Stad',
+    'Email' => 'E-mail',
+    'Fax' => 'Fax',
+    'Firstname' => 'Voornaam',
+    'Language' => 'Taal',
+    'Mobile' => 'Gsm',
+    'Name' => 'Naam',
+    'Phone' => 'Telefoon',
+    'This email address has already been taken.' => 'Dit e-mailadres is al gebruikt.',
+    'This username has already been taken.' => 'Deze gebruikersnaam is al gebruikt.',
+    'Users' => 'Gebruikers',
+    'Zipcode' => 'Postcode',
+];
