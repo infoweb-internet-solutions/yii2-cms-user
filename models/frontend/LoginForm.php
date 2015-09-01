d13752f937646b1e808c594c2a3286717cc65be4
Ruben Heymans <ruben.heymans@gmail.com>
2015-09-01 15:42:28 +0200

    Add translations

--- models/frontend/LoginForm.php
+++ models/frontend/LoginForm.php
@@ -54,6 +54,7 @@ class LoginForm extends Model
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
+
             if (!$user || !$user->validatePassword($this->password)) {
                 $this->addError($attribute, Yii::t('infoweb/user', 'Incorrect username or password'));
             }
